542b2714a720b6659d6ff1fbf5f11695f2e9b005
Jason Crowe <JasonCrowe5@gmail.com>
2021-06-12 07:21:17 -0500

    stopping point

---
+++ FeatureFlagApi/src/FeatureFlagApi/Model/SearchableTag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeatureFlagApi.Model
+{
+    public class SearchableTag
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        //Todo make castable later
+        //TODO: expand to a rules engine perhaps?
+    }
+
+}
